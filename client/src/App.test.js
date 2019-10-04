75e12409da38b321d9018c01b27cf6a911da982d
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-04 12:20:33 -0400

    Added test checking for Soccer header in Navbar

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {render} from '@testing-library/react'
-
+import Navbar from './components/Navbar.js'
 
 test ('App renders without crashing', () => {
   render (<App/>)
 })
 
-test ('Players header found', () =>{
+test ('Players header found in App.js', () =>{
   const {getByText} = render (<App/>);
   getByText (/Players/i)
+})
+
+
+
+test ('Soccer header found in Navbar.js', () =>{
+  const {getByText} = render (<Navbar/>);
+  getByText (/Soccer/i)
 })
